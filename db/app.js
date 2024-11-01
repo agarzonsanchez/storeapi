16814f419083fb206d0d462ca7fa4449c992af0f
Andres Garzon <agarzonsanchez90@gmail.com>
2024-11-01 17:14:56 -0400

    node project created. middleware error handlers working

---
+++ db/app.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
